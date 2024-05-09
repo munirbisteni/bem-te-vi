0e141fbf60b85fc8ac436352085ce680645e01c5
Gustavo Cruz <gustavoindeolcruz@gmail.com>
2024-05-09 17:38:51 -0300

    Adiciona boilerplate inicial da API

---
+++ api/Services/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+
+namespace api.Services;
+
+public interface IPostService
+{
+    Task CreatePost(Post post);
+    Task<Post?> UpdatePost(int id, Post post);
+    Task<Post?> GetPost(int id);
+    Task<List<Post?>> GetAllPosts();
+    Task DeletePost(int id);
+}
