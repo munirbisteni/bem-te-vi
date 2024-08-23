f64e6c33c9aa9abcf1609675a3b7ec140e8c1cbf
munirbisteni <munir.mlcb@gmail.com>
2024-08-22 22:31:08 -0300

    Reestruturação do código

---
+++ backend/old-api/api/Infra/DatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Infra;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string AccountsCollectionName { get; set; } = null!;
+
+    public string PostsCollectionName { get; set; } = null!;
+}
---
+++ backend/old-api/api/Services/AccountsService.cs
@@ -0,0 +1,30 @@
+﻿using api.Infra;
+using api.Models;
+
+using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+namespace api.Services;
+
+public class AccountsService(
+    IOptions<DatabaseSettings> databaseSettings,
+    IMongoDatabase database)
+{
+    private readonly IMongoCollection<Account> _accountsCollection = database.GetCollection<Account>(databaseSettings.Value.AccountsCollectionName);
+
+    public async Task<List<Account>> GetAccounts() =>
+        await _accountsCollection.Find(_ => true).ToListAsync();
+
+    public async Task<Account?> GetAccount(string id) =>
+        await _accountsCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAccount(Account newAccount) =>
+        await _accountsCollection.InsertOneAsync(newAccount);
+
+    public async Task UpdateAccount(string id, Account updatedAccount) =>
+        await _accountsCollection.ReplaceOneAsync(x => x.Id == id, updatedAccount);
+
+    public async Task DeleteAccount(string id) =>
+        await _accountsCollection.DeleteOneAsync(x => x.Id == id);
+}
